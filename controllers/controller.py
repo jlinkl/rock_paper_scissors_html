2224c852c01cc45dd86e8d8eaf800dcd85f23b36
James <james.m.linklater@gmail.com>
2022-07-09 13:45:51 +0100

    updated winner.html

--- controllers/controller.py
+++ controllers/controller.py
@@ -9,11 +9,11 @@ def index():
 @app.route('/<choice1>', methods=['POST'])
 def on_click1(choice1):
     global player1 
-    player1 = Player(request.data['name'], choice1)
+    player1 = Player(request.form['name'], choice1)
     return render_template('player2.html', result1=choice1)
 
 @app.route('/<choice1>/<choice2>', methods=['POST'])
 def on_click2(choice1, choice2):
     player2 = Player(request.form['name'], choice2)
-    return render_template('winner.html', winner=find_winner(player1,player2))
+    return render_template('winner.html', winner=find_winner(player1,player2), first=player1, second=player2)
 
--- models/player.py
+++ models/player.py
@@ -2,7 +2,3 @@ class Player():
     def __init__(self, _name, _choice):
         self.name = _name
         self.choice = _choice
-        self.players = []
-
-    def add_to_list(self, player):
-        self.player.append(player)
